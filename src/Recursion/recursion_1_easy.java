0c408ca1a429a7e8aa3aac1502a71eb610e77e9b
saileshchikkam <sailesh.ch118@gmail.com>
2026-09-22 15:58:40 +0530

    Recursion Easy question done

--- src/Recursion/recursion_1_easy.java
+++ src/Recursion/recursion_1_easy.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import org.w3c.dom.ls.LSOutput;
+
 //recursion with simple example
 public class recursion_1_easy {
     public static void printnumber5to1(int n) {
@@ -26,10 +28,46 @@ public class recursion_1_easy {
         sumoffirstnnumbers(i+1,n,sum);
 
     }
+    public static void PrintFactorialofNumber(int n, int factorial){
+        if(n==0){
+            System.out.println(factorial);
+            return;
+        }
+        factorial *= n;
+        PrintFactorialofNumber(n-1, factorial);
+    }
+
+    public static void fibanacciSeries(int first, int second, int n, int i){
+        if(n == i){
+            return;
+        }
+        System.out.print(first  + " ");
+        fibanacciSeries(second, first+second, n, i+1);
+    }
+
+    public static void decimalToBinary(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        decimalToBinary(n / 2);
+        System.out.print(n % 2);
+    }
+    public static int stringLength(String str, int i) {
+        if (i == str.length()) {
+            return 0;
+        }
+        return 1 + stringLength(str, i + 1);
+    }
     static void main() {
         int n =5;
         printnumber5to1(n);
         printnumber1to5(1);
         sumoffirstnnumbers(1,5,0);
+        PrintFactorialofNumber(3,1);
+        fibanacciSeries(0,1,10, 1);
+        decimalToBinary(10);
+        String str = "Sailesh";
+        System.out.println("\nLength = " + stringLength(str, 0));
     }
 }
